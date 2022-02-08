4fad50fc156b42590dd7f88f1379072ed76bcec4
Moatsem <m3tsmsh@gmail.com>
2022-02-08 04:11:51 +0200

    fetching the data

--- src/component/dashboard.js
+++ src/component/dashboard.js
@@ -1,13 +1,28 @@
-function Dashboard(){
-    return(
-        <div className="dashboard">
+import React, { useState, useEffect } from "react";
 
+function Dashboard() {
+  const [data, setData] = useState([]);
 
-            <h2>Edvora</h2>
-            <h5>Products</h5>
-        </div>
-    )
+  useEffect(async () => {
+     await fetch("https://assessment-edvora.herokuapp.com")
+      .then((response) => response.json())
+      .then((data) => {
+        setData(() => data);
+        console.log(data);
+      });
+  }, []);
+
+  return (
+    <div className="dashboard">
+      <h2>Edvora</h2>
+      <h5>Products</h5>
+      <ol>
+        {data.map((product,index) => (
+          <li key={index}> {product.product_name} </li>
+        ))}
+      </ol>
+    </div>
+  );
 }
 
-
-export default Dashboard
+export default Dashboard;
