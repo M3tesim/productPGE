82b4f35db87d9a01615669d7de88185c9ddaa3f0
Moatsem <m3tsmsh@gmail.com>
2022-02-08 03:05:13 +0200

    first init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Dashboard from './component/dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <div>
+        <Dashboard/>
+      </div>
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/dashboard.js
@@ -0,0 +1,13 @@
+function Dashboard(){
+    return(
+        <div className="dashboard">
+
+
+            <h2>Edvora</h2>
+            <h5>Products</h5>
+        </div>
+    )
+}
+
+
+export default Dashboard
